5712434c78e1ec03f3cd3dd8e8f02a1af35d60dc
kdredge7 <dre17004@byui.edu>
2022-10-08 16:58:18 -0600

    Week 4 Assignment

---
+++ chamber/scripts/date.js
@@ -0,0 +1,5 @@
+const date = {month:"long", day:"numeric", year:"numeric"};
+    document.getElementById("date").textContent = new Date().toLocaleDateString("en-US", date);   
+
+    const date = {month:"long", day:"numeric", year:"numeric"};
+    document.getElementById("date").textContent = new Date().toLocaleDateString("en-US", date);   

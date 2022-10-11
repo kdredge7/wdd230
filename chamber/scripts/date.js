187d2871e2a4d6178de50ea5ad5016ca0218ad07
kdredge7 <dre17004@byui.edu>
2022-10-10 21:21:55 -0600

    JS Edits

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -1,2 +1,9 @@
-const date = {month:"long", day:"numeric", year:"numeric"};
+    const date = {month:"long", day:"numeric", year:"numeric"};
     document.getElementById("date").textContent = new Date().toLocaleDateString("en-US", date);   
+
+    const d = new Date();
+    d.setFullYear(2022);
+    document.getElementById("year").textcontent = d;
+
+    let text = document.lastModified;
+    document.getElementById("modDate").textContent = text;
--- chamber/scripts/date2.js
+++
@@ -1,2 +0,0 @@
-const date = {month:"long", day:"numeric", year:"numeric"};
-    document.getElementById("date2").textContent = new Date().toLocaleDateString("en-US", date);   

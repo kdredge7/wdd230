7645dc69394a43caf59941ff2b233e2e42a6b8b8
kdredge7 <dre17004@byui.edu>
2022-10-15 17:49:57 -0600

    Week 5 Edits

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -1,9 +1,27 @@
-    const date = {month:"long", day:"numeric", year:"numeric"};
+   /* Announcement Bar */ 
+   const meeting = document.getElementById("announcement").textcontent;
+   
+   const day = new Date();
+   currentDate = date.getDay();
+
+   if (currentDate == 1 || currentDate == 2) {
+         meeting.style.display = block;
+    } 
+    else {
+         meeting.style.display = none;
+    }
+   
+    
+
+    /* Date Banner */
+    const date = {weekday:"long", month:"long", day:"numeric", year:"numeric"};
     document.getElementById("date").textContent = new Date().toLocaleDateString("en-US", date);   
 
+    /* Copyright Year */
     const d = new Date();
     d.setFullYear(2022);
     document.getElementById("year").textcontent = d;
 
+    /* Last day Modified */
     let text = document.lastModified;
     document.getElementById("modDate").textContent = text;

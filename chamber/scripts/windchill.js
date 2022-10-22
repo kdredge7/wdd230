c69f103de6d83c6f639f64eef081c31cd32a758a
kdredge7 <dre17004@byui.edu>
2022-10-22 14:25:58 -0600

    Week 6

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -2,17 +2,40 @@
 	 * Input: temperature and wind speed
 	 * Processing: compute temperature (Fahrenheit) with the wind speed (mph)
  	 * Output: The temperature with the wind chill */
- 	
-
-        const tempF = parseFloat(document.querySelector("#tempF").textContent);
+    
+    function chillF(tempF, speed) {
+        const tempF = (document.querySelector("#tempF").textContent);
+        const temp =  parseFloat(tempF);
         const speed = parseFloat(document.querySelector("#speed").textContent);
-        finalTemp = 35.74 + .6215 * tempF - 35.75 * speed ** .16 + .4275 * tempF * speed ** .16;
+        const windspeed =  parseFloat(speed);
+        const windchill = document.querySelector("#windchill");
         
-        if (tempF <= 50 && speed >3) {
-            finalTemp= finalTemp.toFixed(1)
+        if (temp <= 50 && windspeed >3) {
+            const finalTemp = (35.74 + .6215 * temp) - (35.75 * windspeed ** .16) + (.4275 * temp * windspeed ** .16).toFixed(1);
+            return finalTemp + '°F';
         }  
         else {
             finalTemp = "n/a"
         }
+    }
+    document.getElementById("#windChill").innerHTML = chillF
+
         
-        document.getElementById("#windChill").innerHTML = finalTemp;
+        
+       /* function chillF(temperature, windSpeed) {
+            const tempF = document.getElementById("temperatureF").textContent;
+            const temp = parseInt(tempF);
+            const windS = document.getElementById("windspeed").textContent;
+            const wind = parseInt(windS);
+            if (temp <= 50 && wind > 3) {
+              const chilly = (35.74 + (0.6215 * temp) - (35.75 * wind**0.16) + (0.4275 * temp * wind ** 0.16)).toFixed(1);
+              return chilly + `°F`;
+            } else {
+              return "N/A"
+            }
+          } 
+        
+          document.getElementById("#windChill").innerHTML = chillF*/
+
+          
+    

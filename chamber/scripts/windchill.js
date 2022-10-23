337af58d1b6c33906225ebd6263fb061d5e620b9
kdredge7 <dre17004@byui.edu>
2022-10-22 22:05:20 -0600

    responsive design

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -22,20 +22,7 @@
 
         
         
-       /* function chillF(temperature, windSpeed) {
-            const tempF = document.getElementById("temperatureF").textContent;
-            const temp = parseInt(tempF);
-            const windS = document.getElementById("windspeed").textContent;
-            const wind = parseInt(windS);
-            if (temp <= 50 && wind > 3) {
-              const chilly = (35.74 + (0.6215 * temp) - (35.75 * wind**0.16) + (0.4275 * temp * wind ** 0.16)).toFixed(1);
-              return chilly + `°F`;
-            } else {
-              return "N/A"
-            }
-          } 
-        
-          document.getElementById("#windChill").innerHTML = chillF*/
+       
 
           
     

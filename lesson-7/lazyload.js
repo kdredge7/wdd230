c4fcd4f833bd606bf1088ea1d55670ac919fb8d7
kdredge7 <dre17004@byui.edu>
2022-10-26 16:09:22 -0600

    Lesson 7

--- lesson-7/lazyload.js
+++ lesson-7/lazyload.js
@@ -3,14 +3,35 @@ const imagesToLoad = document.querySelectorAll("img[data-src]");
 
 //parameter for Intersectional for when to start loading
 const imgOptions = {
-    rootMargin: '0 0 50px 0',
+    rootMargin:  '0px 0px 50px 0px',
     threshold: 1 //.5
 };
 
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
-    image.onloadn = () => {
+    image.onload = () => {
             image.removeAttribute('data-src');
     };
 };
+
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, imgObserver) => {
+        items.forEach(item => {
+                if (item.isIntersecting) {
+                    loadImages(item.target);
+                    imgObserver.unobserve(item.target);
+                }
+        });
+    }, imgOptions);
+
+
+imagesToLoad.forEach(img => {
+    imgObserver.observe(img);
+});
+} else {// just load All images if not supported
+    imagesToLoad.forEach(img => {
+        loadImages(img);
+    });
+}
+
 

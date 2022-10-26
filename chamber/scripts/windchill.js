0b01f560010e2287e18d1e35eea6d07ddbd1d67b
kdredge7 <dre17004@byui.edu>
2022-10-26 13:39:03 -0600

    large

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,24 +1,3 @@
-/* Defining Table
-	 * Input: temperature and wind speed
-	 * Processing: compute temperature (Fahrenheit) with the wind speed (mph)
- 	 * Output: The temperature with the wind chill 
-    
-    function chillF(temperature, speed) {
-        const tempatureF = (document.querySelector("#tempF").textContent);
-        const temp =  parseFloat(tempatureF);
-        const windS = parseFloat(document.querySelector("#speed").textContent);
-        const windspeed =  parseFloat(windS);
-        const windchill = document.querySelector("#windchill");
-        
-        if (temp <= 50 && windspeed >3) {
-            const finalTemp = (35.74 + .6215 * temp) - (35.75 * windspeed ** .16) + (.4275 * temp * windspeed ** .16).toFixed(1);
-            return finalTemp + '°F';
-        }  
-        else {
-            finalTemp = "n/a"
-        }
-    }
-    document.getElementById("#windChill").innerHTML = chillF(temperature, speed)*/
 
     function chillF(temperature, windSpeed) {
         const tempF = document.getElementById("temperatureF").textContent;

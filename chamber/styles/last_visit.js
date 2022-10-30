90cc81c5ac0c6b5606cc2df5a2a09ae4946bffd2
kdredge7 <dre17004@byui.edu>
2022-10-29 22:32:46 -0600

    edits

--- chamber/styles/last_visit.js
+++ chamber/styles/last_visit.js
@@ -1,7 +1,11 @@
  /* Calculate the number of days since last visited*/
-let date1 = Date.now();
+const date1 = Date.now();
 window.localStorage.setItem("date", date1.getTime());
+const date2 = new Date(Number(window.localStorage.getItem("date")));
+const oneDay = 24 * 60 * 60 * 1000;
 
-let date2 = new Date(Number(window.localStorage.getItem("date")));
+const lastVisit = Math.round(Math.abs((date1 - date2) / oneDay));
 
 document.getElementById("lastVisit").textContent = text;
+
+

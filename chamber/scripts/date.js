b1f2fd538e979cbe40fa81e82ed99ec593b5503a
kdredge7 <dre17004@byui.edu>
2022-11-05 17:13:21 -0600

    form

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -1,12 +1,18 @@
-    /* Date Banner */
+    // Date Banner and Current Date on Join Page
     const date = {weekday:"long", month:"long", day:"numeric", year:"numeric"};
     document.getElementById("date").textContent = new Date().toLocaleDateString("en-US", date);   
 
-    /* Copyright Year */
+    // Current Time
+    var hours = d.getHours();
+    var mins = d.getMinutes();
+    var seconds = d.getSeconds();
+    document.getElementById("time").value = hours + ":" + mins + ":" + seconds;
+
+    //Copyright Year 
     const d = new Date();
     d.setFullYear(2022);
     document.getElementById("year").textcontent = d;
 
-    /* Last day Modified */
+    // Last day Modified 
     let text = document.lastModified;
     document.getElementById("modDate").textContent = text;

79504c4bd8582f7819d0d89501d62a37bd4401f8
kdredge7 <dre17004@byui.edu>
2022-11-17 22:19:02 -0700

    Edits

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,20 +1,20 @@
 
     function chillF(temperature, windSpeed) {
-        const tempF = document.getElementById("temperatureF").textContent;
+        const tempF = document.getElementById("current-temp").textContent;
         const temp = parseInt(tempF);
-        const windS = document.getElementById("windspeed").textContent;
+        const windS = document.getElementById("wind-speed").textContent;
         const wind = parseInt(windS);
         if (temp <= 50 && wind > 3) {
           const chilly = (35.74 + (0.6215 * temp) - (35.75 * wind**0.16) + (0.4275 * temp * wind ** 0.16)).toFixed(0);
           return chilly + `°F`;
         } else{
-          return "N/A"
+          return "N/A";
         }
       }
       
-      document.getElementById("windchill").innerHTML = chillF(temperatureF, windspeed);
+      document.getElementById("windchill").innerHTML =  chillF(temperature, windSpeed);
          
-    
+
     
 
           

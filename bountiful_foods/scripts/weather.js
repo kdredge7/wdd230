351d81a5b4b2ffabc6cf92d4b2f87123ba15ae3b
kdredge7 <dre17004@byui.edu>
2022-12-08 14:39:37 -0700

    Large viewport

--- bountiful_foods/scripts/weather.js
+++ bountiful_foods/scripts/weather.js
@@ -1,10 +1,8 @@
 // select HTML elements in the document
 const currentTemp = document.querySelector('#current-temp');
 const currentCondition = document.querySelector('#currentCond')
-const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const humidity = document.querySelector('#current-humidity');
-const windChill = document.querySelector('#windchill');
+const currenthumidity = document.querySelector('#current-humidity');
 
 // Weather Data
 const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=Carlsbad&units=imperial&appid=79dabcb9110b08edb5788d12143addcb';
@@ -20,7 +18,7 @@ async function apiFetch() {
       console.log(`Response not OK ${await response.text()}`);
     }
   } catch (error) {
-      console.log(`Error: ${error.message}`);
+      // console.log(`Error: ${error.message}`);
   }
 }
   
@@ -32,31 +30,16 @@ function capitalize(string) {
 
 function displayResults(weatherData) {
   let temp = weatherData.main.temp;
-  let speed = weatherData.wind.speed;
-  let iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;
   let desc = weatherData.weather[0].description;
-  let hum = 
+  let humidity = weatherData.main.humidity;
 
   currentTemp.innerHTML = `<strong>${temp.toFixed(0)}</strong>`;
   currentCondition.innerHTML = capitalize(desc);
   windSpeed.innerHTML = `<strong>${speed.toFixed(0)}</strong>`;
   weatherIcon.setAttribute('src', iconsrc);
   weatherIcon.setAttribute('alt', desc);
-  captionDesc.textContent = desc;
-  windChill.innerHTML = chillF(temp, speed);
-}
-   
-   
-   
-function chillF(temp, speed) {
-  if (temp <= 50 && speed > 3) {
-    const chill = (35.74 + (0.6215 * temp) - (35.75 * speed**0.16) + (0.4275 * temp * speed ** 0.16)).toFixed(0);
-    return `${chill} °F`;
-  } else {
-    return "N/A"
-  }
-} 
-      
+  currenthumidity.innerHTML = (humidity);
+}  
      
          
 
